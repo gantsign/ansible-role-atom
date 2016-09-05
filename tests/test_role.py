372ecbdd52a9a1bf95f34f5899b31d0f33ae031e
John Freeman <john.freeman.opensource@gmail.com>
2016-09-05 22:59:36 +0100

    Upgraded to Molecule 1.10.3 (#19)
    Fixed flake8 linter warning.

--- tests/test_role.py
+++ tests/test_role.py
@@ -6,5 +6,6 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_atom(Command):
     assert Command('which atom').rc == 0
 
+
 def test_apm(Command):
     assert Command('apm --version').rc == 0
