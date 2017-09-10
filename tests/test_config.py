e2e5a693d954a568d1051f689452521ebc67fb0c
John Freeman <john.freeman.opensource@gmail.com>
2017-09-10 08:29:37 +0000

    Made config overwrite preference per user
    You now specify whether the Atom config is overwritten on a per user basis.

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,3 +12,34 @@ def test_config(File):
     assert config_file.group == 'test_usr'
     assert oct(config_file.mode) == '0600'
     assert config_file.contains('"projectHome": "/home/vagrant/workspace"')
+
+
+def test_perserve_config(File):
+    config_file = File('/home/test_usr4/.atom/config.cson')
+
+    assert config_file.exists
+    assert config_file.is_file
+    assert config_file.user == 'test_usr4'
+    assert config_file.group == 'test_usr4'
+    assert oct(config_file.mode) == '0600'
+    assert config_file.contains('Existing config')
+
+
+def test_overwrite_config(Command, File):
+    config_file = File('/home/test_usr5/.atom/config.cson')
+
+    assert config_file.exists
+    assert config_file.is_file
+    assert config_file.user == 'test_usr5'
+    assert config_file.group == 'test_usr5'
+    assert oct(config_file.mode) == '0600'
+    assert config_file.contains('"projectHome": "/home/vagrant/workspace"')
+
+    backup_path = Command.check_output('find %s | grep --color=never -E %s',
+                                       '/home/test_usr5/.atom/',
+                                       '~$')
+    backup_file = File(backup_path)
+
+    assert backup_file.is_file
+    assert oct(backup_file.mode) == '0600'
+    assert backup_file.contains('Existing config')
