b37e7ae4dacd085aeff4f7a60e8000c8da6c37fe
John Freeman <john.freeman.opensource@gmail.com>
2018-08-27 12:19:48 +0100

    Changed tests to use host fixture (#60)
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_install.py
+++ molecule/default/tests/test_install.py
@@ -6,9 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_atom(Command):
-    assert Command('which atom').rc == 0
+def test_atom(host):
+    assert host.run('which atom').rc == 0
 
 
-def test_apm(Command):
-    assert Command('apm --version').rc == 0
+def test_apm(host):
+    assert host.run('apm --version').rc == 0
