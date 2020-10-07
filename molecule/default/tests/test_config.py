3b5f2deed37a36c23cafbf62063464b6a3bc6270
John Freeman <john.freeman.opensource@gmail.com>
2020-10-07 22:02:43 +0100

    Removed unnecessary prolog from pytest files (#84)
    Made redundant by `conftest.py`.

--- molecule/default/tests/test_config.py
+++ molecule/default/tests/test_config.py
@@ -1,11 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
 def test_config(host):
     config_file = host.file('/home/test_usr/.atom/config.cson')
 
